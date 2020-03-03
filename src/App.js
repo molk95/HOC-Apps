540980307e0b8b5551f94d5d8b3bdf424956a430
molk95 <molk.saouabi@gmail.com>
2020-03-03 14:00:23 +0100

    cool methode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-
 import './App.css';
-// import ClassCounter from './components/ClassCounter';
-// import HookCounter from './components/HookCounter';
-import HookCounterTwo from './components/hook/HookCounterTwo';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <ClassCounter/> */}
-      {/* <HookCounter/> */}
-      <HookCounterTwo/>
+     
       </header>
     </div>
   );
